1c64117158c057e3b4bbf4ff8e95fb7bc9702993
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 16:57:45 -0700

    fixed typo

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
-db.Homes = require("./homes.models.js")(mongoose);
+db.Homes = require("./homes.model.js")(mongoose);
 
 module.exports = db;
