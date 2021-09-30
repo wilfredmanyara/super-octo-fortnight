3519025d2c6e3d2beb434a79aec7b05bf6d6dbab
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 16:43:55 -0700

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,27 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
+const db = require("./app/models")
+
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        usUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("connected to database");
+    })
+    .catch(err => {
+        console.log("Error: Cannot connect to the database...see: ", err);
+        process.exit();
+    });
+
+// Test Route For Server
 app.get("/", (req, res) => {
     res.json({message: "Welcome to the backend server"});
 });
+
+require("./app/routes/homes.routes")(app);
 
 const PORT = process.env.PORT || 8080;
 
