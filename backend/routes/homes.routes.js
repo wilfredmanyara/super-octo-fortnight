64a1b99fa5316debddc2d3afea3170200a27b73e
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 16:11:07 -0700

    added routes for get all homes endpoint

---
+++ backend/routes/homes.routes.js
@@ -0,0 +1,11 @@
+module.exports = app => {
+    const homes = require("../controllers/homes.controllers.js");
+
+    var router = require("express").Router();
+
+    //Retrieve All Houses
+    router.get("/", homes.findAll);
+
+
+    app.use("/api/homes", router);
+}
