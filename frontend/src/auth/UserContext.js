df53114cbbffac6a6e908bcb987700f080bdba1e
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 13:31:00 -0700

    added user context

---
+++ frontend/src/auth/UserContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//Context provides the current user object and the setter for checking auth in app
+
+const UserContext = React.createContext();
+
+export default UserContext;
