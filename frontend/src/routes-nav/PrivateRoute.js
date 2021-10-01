c41031ed4d5a7f2348a488f58ce429aa24952f55
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 12:26:40 -0700

    added comment

--- frontend/src/routes-nav/PrivateRoute.js
+++ frontend/src/routes-nav/PrivateRoute.js
@@ -2,7 +2,8 @@ import React, { useContext } from 'react';
 import { Router, Redirect } from 'react-router-dom';
 import UserContext from '../../auth/UserContext';
 
-
+// Higher order component for checking if user is logged in.
+// If not, client is redirected to login
 
 function PrivateRoute({ exact, path, children }) {
     const { currentUser } = useContext(UserContext);
