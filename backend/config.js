4f89598e397175d1bbe456d7782082aeb08b6b66
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 16:27:32 -0700

    added config to backend

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-key-dev";
+
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
+
+module.exports = {
+    SECRET_KEY,
+    BCRYPT_WORK_FACTOR
+}
