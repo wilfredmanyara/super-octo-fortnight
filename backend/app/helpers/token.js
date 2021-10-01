f9817337caafeb89765d7aed875f4a538f346e4d
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 16:28:34 -0700

    added ability to create token on backend

---
+++ backend/app/helpers/token.js
@@ -0,0 +1,16 @@
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../../config");
+
+/** Sign the token and return it */
+
+function createToken(user) {
+
+    let payload = {
+        username: user.username,
+        isAdmin: user.isAdmin || false
+    };
+
+    return jwt.sign(payload, SECRET_KEY);
+}
+
+module.exports = { createToken }
