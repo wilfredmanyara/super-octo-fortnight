15e57febe1c952f5018585a128e82d6ad1481b06
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 18:29:19 -0700

    fixed typo

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,7 +4,6 @@ const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8080"
 
 class HomesApi {
 
-
     //General request Method
     static async request(endpoint, data = {}, method = "get") {
         console.log("API Call:", endpoint, data, method);
@@ -27,7 +26,7 @@ class HomesApi {
     /**  Get all of the homes from server */
 
     static async getAllHomes() {
-        let res = await this.request(`/api/homes`);
+        let res = await this.request(`api/homes`);
         console.log("Data from server", res);
         return res;
     }
@@ -35,7 +34,7 @@ class HomesApi {
     /** Post a new home */
 
     static async postAHome(data) {
-        let res = await this.request(`/api/homes`, data, "post");
+        let res = await this.request(`api/homes`, data, "post");
         console.log("Data from server", res);
         return res;
     }
