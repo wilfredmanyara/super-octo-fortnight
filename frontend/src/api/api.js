826138ac3d9eb84a078f459991e57a2ffa6d0702
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 17:50:45 -0700

    added HomeApi classes for network requests

---
+++ frontend/src/api/api.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8080"
+
+class HomesApi {
+
+
+    //General request Method
+    static async request(endpoint, data = {}, method = "get") {
+        console.log("API Call:", endpoint, data, method);
+
+        const url = `${BASE_URL}/${endpoint}`;
+        const headers = {};
+        const params = (method === "get") ? data : {};
+
+        try {
+            return (await axios({url, method, data, params, headers})).data;
+        } catch(err) {
+            console.log("API Error:", err.response);
+            let message = err.response.data.error.message;
+            throw Array.isArray(message) ? message : [message];
+        }
+    }
+
+    // ROUTES:
+
+    /**  Get all of the homes from server */
+
+    static async getAllHomes() {
+        let res = await this.request(`/api/homes`);
+        console.log("Data from server", res);
+        return res;
+    }
+
+    /** Post a new home */
+
+    static async postAHome(data) {
+        let res = await this.request(`/api/homes`, data, "post");
+        console.log("Data from server", res);
+        return res;
+    }
+
+}
+
+export default HomesApi;
