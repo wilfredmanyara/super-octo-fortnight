9f1b5b0abfd8d954a1382173fd918be78b0d98f8
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 13:32:03 -0700

    added authentication calls to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
-import HomesList from './homes/homesList';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import useLocalStorage from './hooks/useLocalStorage';
+import UserContext from './auth/UserContext';
+import HomesApi from './api/api';
+import jwt from 'jsonwebtoken';
+import LoadingSpinner from './common/LoadingSpinner';
+import Routes from './routes-nav/Routes';
+
+export const TOKEN_STORAGE_ID = 'hearth-token';
 
 function App() {
+
+  const [infoLoaded, setInfoLoaded] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
+  const [token, setToken] = useLocalStorage(TOKEN_STORAGE_ID);
+
+  useEffect(function loadUserInfo() {
+
+    async function getCurrentUser() {
+      if (token) {
+        try {
+          let { username } = jwt.decode(token);
+
+          HomesApi.token = token;
+          let currentUser = await HomesApi.getCurrentUser(username);
+          setCurrentUser(currentUser);
+        } catch (err) {
+          console.log("error: ", err);
+          setCurrentUser(null);
+        }
+      }
+      setInfoLoaded(true);
+    }
+    setInfoLoaded(false);
+    getCurrentUser();
+  }, [token]);
+
+  function logout() {
+    setCurrentUser(null);
+    setToken(null);
+  }
+
+  async function signup(signupData) {
+    try {
+      let token = await HomesApi.signup(signupData);
+      setToken(token);
+      return {success: true};
+    } catch (errors) {
+      console.log("sign up error: ", errors)
+      return { success: false, errors }
+    }
+  }
+
+  async function login(loginData) {
+    try {
+      let token = HomesApi.login(loginData);
+      setToken(token);
+      return { success: true };
+    } catch (errors) {
+      console.log("login failed", errors);
+      return { success: false, errors };
+    }
+  }
+
+  if (!infoLoaded) return <LoadingSpinner />
+
   return (
-    <div className="App">
-      <HomesList />
-    </div>
+    <BrowserRouter>
+      <UserContext.Provider
+          value={{ currentUser, setCurrentUser }}>
+        <div className="App">
+          <Navigation logout={logout} />
+          <Routes login={login} signup={signup} />
+        </div>
+      </UserContext.Provider>
+    </BrowserRouter>
   );
 }
 
