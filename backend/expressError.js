fc3f0de9c26dfc36c5546cbc4ce9ce7d7ad07ef3
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 16:27:47 -0700

    added error classes

---
+++ backend/expressError.js
@@ -0,0 +1,50 @@
+
+
+
+class ExpressError extends Error {
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+// Raise 404 error
+
+class NotFoundError extends ExpressError {
+    constructor(message = "Not Found") {
+        super(message, 404);
+    }
+}
+
+// Raise 401 - Unauthorized
+
+class UnauthorizedError extends ExpressError {
+    constructor(message = "Unauthorized") {
+        super(message, 401);
+    }
+}
+
+// Raise 400 - Bad Request
+
+class BadRequestError extends ExpressError {
+    constructor(message = "Bad Request") {
+        super(message, 400)
+    }
+}
+
+// Raise 403 - Forbidden Error
+
+class ForbiddenError extends ExpressError {
+    constructor(message = "Bad Request") {
+        super(message, 403);
+    }
+}
+
+module.exports = {
+    ExpressError,
+    NotFoundError,
+    UnauthorizedError,
+    BadRequestError,
+    ForbiddenError
+}
