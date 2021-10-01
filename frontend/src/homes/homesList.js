2eac3631cfc1ef6ab55667cf365c512e0001e4bb
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-09-30 18:51:22 -0700

    added homeCard component

--- frontend/src/homes/homesList.js
+++ frontend/src/homes/homesList.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import LoadingSpinner from '../common/LoadingSpinner';
 import HomesApi from "../api/api";
+import HomeCard from './homeCard';
 
 function HomesList() {
 
     const [homes, setHomes] = useState(null);
 
-    useEffect(async function getHomes() {
-        console.log("Getting Companies Now...");
-        let homesToAdd = await HomesApi.getAllHomes();
-        setHomes(homesToAdd);
+    useEffect(() => {
+        async function getHomes() {
+            console.log("Getting Companies Now...");
+            let homesToAdd = await HomesApi.getAllHomes();
+            setHomes(homesToAdd);
+        }
+        getHomes()
     }, []);
 
     if (!homes) return <LoadingSpinner />
 
     return(
-        <h1>hello world</h1>
+        <div>
+            <h1>Home List Below</h1>
+            {
+                homes.map(home => (
+                    <HomeCard home={home} />
+                ))
+            }
+        </div>
     )
 }
 
