b235bef2090c2d104d3e82f7c55e8e3d9c67d616
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 13:32:13 -0700

    moved user context

--- frontend/auth/UserContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-//Context provides the current user object and the setter for checking auth in app
-
-const UserContext = React.createContext();
-
-export default UserContext;
