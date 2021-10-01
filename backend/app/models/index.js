19e0d111feb49d95942aafa256f3c9c4eb544e26
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 16:56:29 -0700

    added users to db config

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -7,5 +7,6 @@ const db = {};
 db.mongoose = mongoose;
 db.url = dbConfig.url;
 db.Homes = require("./homes.model.js")(mongoose);
+db.Users = require("./user.model.js")(mongoose);
 
 module.exports = db;
