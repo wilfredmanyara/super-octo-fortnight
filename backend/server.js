d3a74b36d3e6a79423cab3c1b2b46029926796c6
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 16:46:57 -0700

    added routes & error handlers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const { NotFoundError } = require("./expressError");
+
+const { authenticateJWT } = require("./app/middleware/auth");
+const authRoutes = require("./routes/auth.routes.js");
+const homesRoutes = require("./routes/homes.routes.js")
+const usersRoutes = require("./routes/users.routes.js")
+
+const morgan = require("morgan");
+
 const app = express();
 
 var corsOptions = {
@@ -8,13 +17,17 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"))
+app.use(authenticateJWT)
+
+//register routes
+app.use("/auth", authRoutes);
+app.use("/homes", homesRoutes);
+app.use("/users", usersRoutes);
 
 const db = require("./app/models")
-
 db.mongoose
     .connect(db.url, {
         useNewUrlParser: true,
@@ -28,15 +41,26 @@ db.mongoose
         process.exit();
     });
 
-// Test Route For Server
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to the backend server"});
+// handle 404 errors
+app.use(function (req, res, next) {
+    return next(new NotFoundError());
 });
 
-require("./app/routes/homes.routes")(app);
+// require("./app/routes/homes.routes")(app);
+
+// Generic Error Handler
+app.use(function (err, req, res, next) {
+    if (process.env.NODE_ENV !== "test") console.error(err.stack);
+    const status = err.status || 500;
+    const message = err.message;
+
+    return res.status(status).json({
+        error: { message, status },
+    });
+});
 
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`server is listening on PORT ${PORT}`)
-})
+});
