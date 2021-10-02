437107d273483230acbadcc334d5051b1f975a92
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 20:20:18 -0700

    added create and authenticate method

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -6,26 +6,36 @@ const {
     BadRequestError,
     UnauthorizedError
 } = require("../../expressError");
+const userRegisterSchema = require("../schemas/userRegister.json");
+const userAuthSchema = require("../schemas/userAuth.json");
 
 const { BCRYPT_WORK_FACTOR } = require("../../config.js");
+const jsonschema = require("jsonschema");
+const { createToken } = require("../helpers/token");
 
 //Create a New User
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
 
-    let hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+    const validator = jsonschema.validate(req.body, userRegisterSchema);
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+    }
+
+    let hashedPassword = await bcrypt.hash(req.body.password, BCRYPT_WORK_FACTOR);
 
     const newUser = new Users({
                 firstName: req.body.firstName,
                 lastName: req.body.lastName,
                 email: req.body.email,
                 password: hashedPassword,
-                isAdmin: req.body.isAdmin
+                isAdmin: false
     })
+    const token = createToken(newUser)
 
     newUser.save(newUser)
         .then(data => {
-            // res.send(data);
-            return data;
+            res.status(201).send({ token });
         })
         .catch(err => {
             res.status(500).send({
@@ -33,4 +43,38 @@ exports.create = (req, res) => {
                     err.message || "An error occured while creating user"
             })
         })
+}
+
+exports.authenticate = async (req, res, next) => {
+
+    const validator = jsonschema.validate(req.body, userAuthSchema);
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+    };
+
+    const { email, password } = req.body;
+
+    let user = await Users.findOne({
+        email: email
+    });
+
+
+    try {
+        if(user) {
+            const isValid = await bcrypt.compare(password, user.password)
+            if (isValid === true) {
+                delete user[password];
+                const token = createToken(user);
+                return res.status(200).json({ token });
+            } else {
+                throw new UnauthorizedError("Invalid username/password");
+            }
+        }
+    } catch (error) {
+        //TO DO: NEED TO HANDLE ERROR!
+        throw new UnauthorizedError("Invalid username/password");
+    }
+
+
 }
