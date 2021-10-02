5aa2b2b5d22fd56d62aa99810c8a151624ff85d4
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-02 13:12:01 -0700

    added clarifying comments for routes

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -1,42 +1,13 @@
 
-
-
-// const jsonschema = require("jsonschema");
-
-// const Users = require("../models/user.model");
-// const express = require("express");
-// const router = new express.Router();
-// const { createToken } = require("../helpers/token");
-// const userAuthSchema = require("../schemas/userAuth.json");
-// const userRegisterSchema = require("../schemas/userRegister.json");
-// const { BadRequestError } = require("../../expressError");
-
-// POST to /auth/token with { username, password }
-
-// router.post("/token", async function (req, res, next) {
-//     const validator = jsonschema.validate(req.body, userAuthSchema);
-//     if (!validator.valid) {
-//         const errs = validator.errors.map(e => e.stack);
-//         throw new BadRequestError(errs);
-//     };
-
-//     const { username, password } = req.body;
-//     const user = await Users.authenticate(username, password);
-//     const token = createToken(user);
-//     return res.json({ token });
-// });
-
-
-
 module.exports = app => {
     const users = require("../controllers/user.controller.js");
 
     var router = require("express").Router();
 
-    //Retrieve All Houses
+    // Authenticate a User During Login
     router.post("/token", users.authenticate);
 
-    //Post To Register
+    // Register a user during SignUp
     router.post("/register", users.create);
 
     app.use("/api/auth", router);
