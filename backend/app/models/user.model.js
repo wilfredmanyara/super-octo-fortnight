b082b7b26083a0eb2860abc6d90daf6fcd4578fa
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 17:11:32 -0700

    user model added

---
+++ backend/app/models/user.model.js
@@ -0,0 +1,25 @@
+
+module.exports = mongoose => {
+    const schema = 
+        mongoose.Schema(
+            {
+                firstName: String,
+                lastName: String,
+                email: String,
+                password: String,
+                isAdmin: Boolean,
+            },
+            {
+                timestamps: true
+            }
+        );
+
+        schema.method("toJSON", function() {
+            const { __v, _id, ...object } = this.toObject();
+            object.id = _id;
+            return object;
+        })
+
+    const Users = mongoose.model("Users", schema);
+    return Users;
+}
