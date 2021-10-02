69a006bd9660ec2d0e56632d9ae5cb0c3dfd0dc0
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 18:11:29 -0700

    fixed type

--- backend/app/routes/users.routes.js
+++ backend/app/routes/users.routes.js
@@ -5,7 +5,7 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
-const User = require("../models/user");
+const User = require("../models/user.model");
 const { createToken } = require("../helpers/token");
 const userNewSchema = require("../schemas/userNew.json");
 
@@ -21,7 +21,7 @@ router.post("/", ensureAdmin, async function (req, res, next) {
             throw new BadRequestError(errs);
         }
 
-        const user = await User.register(req.body);
+        const user = await User.create(req);
         const token = createToken(user);
         return res.status(201).json({ user, token });
     } catch (err) {
