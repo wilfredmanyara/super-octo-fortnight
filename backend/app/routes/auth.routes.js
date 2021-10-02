f860c4f7906f15a0e1f588f3e14959401672061f
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-01 20:20:43 -0700

    deleted route content

--- backend/app/routes/auth.routes.js
+++ backend/app/routes/auth.routes.js
@@ -1,43 +1,43 @@
 
 
 
-const jsonschema = require("jsonschema");
+// const jsonschema = require("jsonschema");
 
-const User = require("../models/user.model");
-const express = require("express");
-const router = new express.Router();
-const { createToken } = require("../helpers/token");
-const userAuthSchema = require("../schemas/userAuth.json");
-const userRegisterSchema = require("../schemas/userRegister.json");
-const { BadRequestError } = require("../../expressError");
+// const Users = require("../models/user.model");
+// const express = require("express");
+// const router = new express.Router();
+// const { createToken } = require("../helpers/token");
+// const userAuthSchema = require("../schemas/userAuth.json");
+// const userRegisterSchema = require("../schemas/userRegister.json");
+// const { BadRequestError } = require("../../expressError");
 
 // POST to /auth/token with { username, password }
 
-router.post("/token", async function (req, res, next) {
-    const validator = jsonschema.validate(req.body, userAuthSchema);
-    if (!validator.valid) {
-        const errs = validator.errors.map(e => e.stack);
-        throw new BadRequestError(errs);
-    };
+// router.post("/token", async function (req, res, next) {
+//     const validator = jsonschema.validate(req.body, userAuthSchema);
+//     if (!validator.valid) {
+//         const errs = validator.errors.map(e => e.stack);
+//         throw new BadRequestError(errs);
+//     };
 
-    const { username, password } = req.body;
-    const user = await User.authenticate(username, password);
-    const token = createToken(user);
-    return res.json({ token });
-});
+//     const { username, password } = req.body;
+//     const user = await Users.authenticate(username, password);
+//     const token = createToken(user);
+//     return res.json({ token });
+// });
 
-// POST to auth/register: { user } => { token }
 
-router.post("/register", async function (req, res, next) {
-    const validator = jsonschema.validate(req.body, userRegisterSchema);
-    if (!validator.valid) {
-        const errs = validator.errors.map(e => e.stack);
-        throw new BadRequestError(errs);
-    }
 
-    const newUser = await User.register({ ...req.body, isAdmin: false});
-    const token = createToken(newUser);
-    return res.status(201).json({ token });
-});
+module.exports = app => {
+    const users = require("../controllers/user.controller.js");
 
-module.exports = router;
+    var router = require("express").Router();
+
+    //Retrieve All Houses
+    router.post("/token", users.authenticate);
+
+    //Post To Register
+    router.post("/register", users.create);
+
+    app.use("/api/auth", router);
+}
