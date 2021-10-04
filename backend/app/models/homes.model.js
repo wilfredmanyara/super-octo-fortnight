713c6e975b8ceee67a6663c636c39b3e06b5dcbd
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-03 21:42:37 -0700

    updated homes model

--- backend/app/models/homes.model.js
+++ backend/app/models/homes.model.js
@@ -42,6 +42,6 @@ module.exports = mongoose => {
             return object;
         })
 
-    const Homes = mongoose.model("Homes", schema);
+    const Homes = mongoose.model("housing_data", schema);
     return Homes;
 }
