685521ae2b475eb3cf7e8d2dc6efcdb7c550c9cd
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-03 21:08:49 -0700

    added seed file for seeding database

---
+++ backend/seed.js
@@ -0,0 +1,54 @@
+
+const csv = require('csv-parser');
+const fs = require('fs')
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/";
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("./config.js");
+
+//Read in housing data
+fs.createReadStream('redfin_data.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+
+        // Call mongoclient and insert
+        MongoClient.connect(url, function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("test2");
+            var myObj = row;
+            dbo.collection("housing_data").insertOne(myObj, function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            })
+        })
+    })
+    .on('end', () => {
+        console.log('CSV file has been processed')
+    });
+
+//Read in User data
+fs.createReadStream('user_data.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+
+        // Call mongoclient and insert
+        MongoClient.connect(url, async function(err, db) {
+            if (err) throw err;
+            var dbo = db.db("test2");
+            var myObj = row;
+            myObj.password = await bcrypt.hash(myObj.password, BCRYPT_WORK_FACTOR)
+            myObj.isAdmin = true
+            console.log("User to insert: ")
+            console.log(myObj);
+
+            dbo.collection("users").insertOne(myObj, function (err, res) {
+                if (err) throw err;
+                console.log("1 document inserted");
+                db.close();
+            })
+        })
+    })
+    .on('end', () => {
+        console.log('CSV file has been processed')
+    });
