ff06250aee3be8e95569090bc3e83850c1c62a0f
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-04 12:21:01 -0700

    added bearer token for authorization

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,13 +3,14 @@ import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:8080"
 
 class HomesApi {
+    static token;
 
     //General request Method
     static async request(endpoint, data = {}, method = "get") {
         console.log("API Call:", endpoint, data, method);
 
         const url = `${BASE_URL}/${endpoint}`;
-        const headers = {};
+        const headers = { Authorization: `Bearer ${HomesApi.token}`};
         const params = (method === "get") ? data : {};
 
         try {
@@ -50,14 +51,15 @@ class HomesApi {
     /** Get the token for login from username and password */
 
     static async login(data) {
-        let res = await this.request(`auth/token`, data, "post");
+        console.log("data sent is: ", data)
+        let res = await this.request(`api/auth/token`, data, "post");
         return res.token;
     }
 
     /** Signup for the site */
 
     static async signup(data) {
-        let res = await this.request(`auth/register`, data, "post");
+        let res = await this.request(`api/auth/register`, data, "post");
         return res.token;
     }
 
