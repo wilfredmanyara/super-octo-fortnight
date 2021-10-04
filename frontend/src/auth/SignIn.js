c1b8fcd546d8b8b1e9af9a2faec6c5c1216e076b
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-04 12:47:43 -0700

    removed unused imports

--- frontend/src/auth/SignIn.js
+++ frontend/src/auth/SignIn.js
@@ -4,8 +4,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
@@ -14,7 +12,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useHistory } from 'react-router-dom';
-import LoadingSpinner from '../common/LoadingSpinner';
 import Alert from '../common/Alert';
 
 // NOTE: MATERIAL UI TEMPLATE TAKEN FROM TEMPLATE PROVIDED BY MATERIAL UI
