2d16109ccefdea22ab51d9750deaeeb0751467e1
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-05 16:59:17 -0700

    moved HousingCount

---
+++ frontend/src/components/dashboard/HouseCount.js
@@ -0,0 +1,38 @@
+   
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import Title from '../../common/Title';
+import { Grid } from '@mui/material';
+import { Paper } from '@mui/material';
+
+export default function HouseCount(data) {
+
+  let today = new Date();
+
+  return (
+    <Grid item xs={12} md={4} lg={3}>
+      <Paper
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          height: 500,
+        }}
+      >
+        <React.Fragment>
+          <Title>Number Of Listings</Title>
+          <Typography component="p" variant="h4">
+            {data.data.length}
+          </Typography>
+          <Typography color="text.secondary" sx={{ flex: 1 }}>
+            as of {today.toLocaleDateString()}
+          </Typography>
+          <Link color="primary" href="https://www.redfin.com/city/17151/CA/San-Francisco" >
+            View More Houses
+          </Link>
+        </React.Fragment>
+      </Paper>
+    </Grid>
+  );
+}
