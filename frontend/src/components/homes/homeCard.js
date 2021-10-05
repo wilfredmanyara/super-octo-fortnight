f7a9cfcae5b8ba7da08e73154ea15799a146e30b
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-05 16:36:54 -0700

    added homeCard content

--- frontend/src/components/homes/homeCard.js
+++ frontend/src/components/homes/homeCard.js
@@ -6,12 +6,38 @@ function HomeCard({home}) {
 
 
     return (
-        <Card sx={{maxWidth: 345}}>
-            <Typography gutterBottom variant="h5" component="div">
-                {home.address}
+        <Card 
+            sx={{
+                maxWidth: '100%'
+            }} 
+            style={{
+                display: 'flex',
+                width: '100%',
+                margin: '3px',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                textAlign: 'center'
+            }}
+        >
+            
+            <Typography 
+                gutterBottom 
+                variant="h5" 
+                component="div"
+                style={{
+                    display: 'flex',
+                    width: '100%',
+                    margin: '3px',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center'
+                }}
+            >
+                {home.address} - # Beds: {home.beds} - # Baths: {home.baths} 
             </Typography>
         </Card>
-
     )
 }
 
