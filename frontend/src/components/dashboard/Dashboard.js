7aff5d63483b1ddfe1bdcd3fd7fda9eb6b4230d1
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-06 12:54:40 -0700

    Moved content to separate components

--- frontend/src/components/dashboard/Dashboard.js
+++ frontend/src/components/dashboard/Dashboard.js
@@ -11,37 +11,9 @@ import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import { mainListItems } from "../../routes-nav/ListItems";
-import Chart from "./Chart";
-import ListingCount from "./HouseCount";
-import HousingPagination from "./HousingPagination";
-import HomesApi from "../../api/api";
-import LoadingSpinner from "../../common/LoadingSpinner";
-import { useLocation } from "react-router-dom";
-import HomesList from "../homes/homesList";
-
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Hearth
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const drawerWidth = 240;
 
@@ -91,27 +63,8 @@ const Drawer = styled(MuiDrawer, {
 
 const mdTheme = createTheme();
 
-function DashboardContent({ logout }) {
-  const location = useLocation();
+function DashboardContent({ logout, children }) {
   const [open, setOpen] = useState(true);
-  const [houseData, setHouseData] = useState([]);
-  const [infoLoaded, setInfoLoaded] = useState(false);
-
-  React.useEffect(function loadHouseData() {
-    async function getHouseData() {
-      try {
-        let data = await HomesApi.getAllHomes();
-        setHouseData(data);
-      } catch (err) {
-        console.log("error fetching data: ", err);
-      }
-      setInfoLoaded(true);
-    }
-    setInfoLoaded(false);
-    getHouseData();
-  }, []);
-
-  if (!infoLoaded) return <LoadingSpinner />;
 
   const toggleDrawer = () => {
     setOpen(!open);
@@ -182,27 +135,13 @@ function DashboardContent({ logout }) {
           }}
         >
           <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            {location.pathname === "/dashboard" ? (
-              <Grid container spacing={3}>
-                {/* Chart */}
-                <Chart data={houseData} />
-                {/* # Listings */}
-                <ListingCount data={houseData} />
-                {/* Recent Listings */}
-                <HousingPagination data={houseData} />
-              </Grid>
-            ) : (
-              <HomesList data={houseData} />
-            )}
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
+          {children}
         </Box>
       </Box>
     </ThemeProvider>
   );
 }
 
-export default function Dashboard({ logout }) {
-  return <DashboardContent logout={logout} />;
+export default function Dashboard({ logout, children }) {
+  return <DashboardContent logout={logout}>{children}</DashboardContent>;
 }
