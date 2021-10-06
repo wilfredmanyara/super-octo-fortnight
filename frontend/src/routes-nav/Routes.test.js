ed9144e5cd5dce836c0908696175a01577f454af
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-05 17:31:44 -0700

    added route tests

---
+++ frontend/src/routes-nav/Routes.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Routes from './Routes';
+import { MemoryRouter } from 'react-router';
+import { UserProvider } from '../testUtils';
+
+it('renders without crashing', function () {
+    render(
+        <MemoryRouter>
+            <UserProvider>
+                <Routes />
+            </UserProvider>
+        </MemoryRouter>
+    )
+});
