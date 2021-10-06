d95367fcafe41d335012b36755bbbe8afc4f2551
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-05 17:33:05 -0700

    added test utilities file

---
+++ frontend/src/testUtils.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserContext from './auth/UserContext';
+
+const demoUser = {
+    email: "email@gmail.com",
+    firstName: "Hello",
+    lastName: "World",
+    password: "password"
+};
+
+const UserProvider = 
+({ children, currentUser = demoUser}) => (
+    <UserContext.Provider value={{ currentUser }}>
+        { children }
+    </UserContext.Provider>
+);
+
+export { UserProvider };
