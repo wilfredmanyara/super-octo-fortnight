420e4edec9b37fe71b94fa509c2c161a24f826c3
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-07 21:08:44 -0700

    added tests to auth

---
+++ backend/app/routes/auth.routes.test.js
@@ -0,0 +1,42 @@
+
+
+const require = require("supertest");
+const { request } = require("../../app");
+
+const app = require("../../app");
+
+const {
+    commonBeforeAll,
+    commonBeforeEach,
+    commonAfterEach,
+    commonAfterAll
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+commonBeforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+describe("POST to /auth/token", function () {
+    test("works for authorized user", async function() {
+        const resp = await request(app)
+        .post("/auth/token")
+        .send({
+            email: "u1",
+            password: "password1",
+        });
+        expect(resp.body).toEqual({
+            "token": expect.any(String),
+        });
+    });
+
+    test("unauth with non existent user", async function () {
+        const resp = await request(app)
+        .post("/auth/token")
+        .send({
+            email: "fakeUser@gmail.com",
+            password: "password"
+        })
+        expect(resp.statusCode).toEqual(401);
+    })
+});
