f688514c31ecf0b1f99658fa6d23a046a5779179
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-10-07 21:07:36 -0700

    separated db layer into separate file

---
+++ backend/db.js
@@ -0,0 +1,16 @@
+const db = require("./app/models");
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("Error: Cannot connect to the database...see: ", err);
+    process.exit();
+  });
+
+  module.exports = db;
