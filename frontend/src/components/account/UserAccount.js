6b916fb390f3a6752b2cad0faf9765d3fad0ba71
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-11-07 19:29:59 -0800

    changed link name

--- frontend/src/components/account/UserAccount.js
+++ frontend/src/components/account/UserAccount.js
@@ -22,7 +22,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
-        Hearth
+        Full-Stack App Template
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
