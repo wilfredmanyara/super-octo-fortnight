5a074aa3e30fb9a8261fdd80f6f510f35f8aa0ac
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-11-07 20:17:10 -0800

    formatted via prettier

--- backend/app/routes/auth.routes.test.js
+++ backend/app/routes/auth.routes.test.js
@@ -1,15 +1,13 @@
-
-
 const require = require("supertest");
 const { request } = require("../../app");
 
 const app = require("../../app");
 
 const {
-    commonBeforeAll,
-    commonBeforeEach,
-    commonAfterEach,
-    commonAfterAll
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
 } = require("./_testCommon");
 
 beforeAll(commonBeforeAll);
@@ -18,25 +16,21 @@ afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
 describe("POST to /auth/token", function () {
-    test("works for authorized user", async function() {
-        const resp = await request(app)
-        .post("/auth/token")
-        .send({
-            email: "u1",
-            password: "password1",
-        });
-        expect(resp.body).toEqual({
-            "token": expect.any(String),
-        });
+  test("works for authorized user", async function () {
+    const resp = await request(app).post("/auth/token").send({
+      email: "u1",
+      password: "password1",
     });
+    expect(resp.body).toEqual({
+      token: expect.any(String),
+    });
+  });
 
-    test("unauth with non existent user", async function () {
-        const resp = await request(app)
-        .post("/auth/token")
-        .send({
-            email: "fakeUser@gmail.com",
-            password: "password"
-        })
-        expect(resp.statusCode).toEqual(401);
-    })
+  test("unauth with non existent user", async function () {
+    const resp = await request(app).post("/auth/token").send({
+      email: "fakeUser@gmail.com",
+      password: "password",
+    });
+    expect(resp.statusCode).toEqual(401);
+  });
 });
