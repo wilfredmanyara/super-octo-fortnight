f65e9acfcf876d48757fc76e4c0385fb1f350b64
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-11-07 20:16:08 -0800

    formatted via prettier

--- backend/config.js
+++ backend/config.js
@@ -8,24 +8,24 @@ const PORT = +process.env.PORT || 3001;
 // Use the development database, testing database, or production via env
 // variables
 function getDatabaseUri() {
-  return (process.env.NODE_ENV === "test") 
+  return process.env.NODE_ENV === "test"
     ? "app_test"
-    : process.env.DATABASE_URL || "app"
+    : process.env.DATABASE_URL || "app";
 }
 
 // Set bcrypt factor lower during testing
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-console.log("Application Configuration: " .green);
-console.log("SECRET_KEY " .yellow, SECRET_KEY);
-console.log("PORT:" .yellow, PORT.toString());
-console.log("BCRYPT_WORK_FACTOR" .yellow, BCRYPT_WORK_FACTOR);
-console.log("Database:" .yellow, getDatabaseUri());
+console.log("Application Configuration: ".green);
+console.log("SECRET_KEY ".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
 console.log("---");
 
 module.exports = {
   SECRET_KEY,
   BCRYPT_WORK_FACTOR,
   PORT,
-  getDatabaseUri
+  getDatabaseUri,
 };
