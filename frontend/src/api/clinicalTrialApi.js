5fff8b1ccaa20e60ab3d329253748254f89f47e5
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2021-11-07 20:17:35 -0800

    formatted via prettier

--- frontend/src/api/clinicalTrialApi.js
+++ frontend/src/api/clinicalTrialApi.js
@@ -1,29 +1,26 @@
 import axios from "axios";
 
-const BASE_URL_CLINICAL_TRIALS = "https://clinicaltrials.gov/api/query"
+const BASE_URL_CLINICAL_TRIALS = "https://clinicaltrials.gov/api/query";
 
 class ClinicalTrialsApi {
+  static async request(endpoint, data = {}, method = "get") {
+    const url = `${BASE_URL_CLINICAL_TRIALS}/${endpoint}`;
+    const headers = {};
+    const params = method === "get" ? data : {};
 
-    static async request(endpoint, data = {}, method = "get") {
-
-        const url = `${BASE_URL_CLINICAL_TRIALS}/${endpoint}`;
-        const headers = {};
-        const params = method === "get" ? data : {};
-
-        try {
-            return (await axios({ url, method, data, params, headers}))
-        } catch (err) {
-            console.log("API Error:", err.response);
-            let message = err.response.data.error.message;
-            throw Array.isArray(message) ? message : [message];
-        }
+    try {
+      return await axios({ url, method, data, params, headers });
+    } catch (err) {
+      console.log("API Error:", err.response);
+      let message = err.response.data.error.message;
+      throw Array.isArray(message) ? message : [message];
     }
+  }
 
-    static async makeRequest(data) {
-        let res = this.request(`full_studies?expr=cancer&fmt=JSON`);
-        return res;
-    };
-
+  static async makeRequest(data) {
+    let res = this.request(`full_studies?expr=cancer&fmt=JSON`);
+    return res;
+  }
 }
 
 export default ClinicalTrialsApi;
